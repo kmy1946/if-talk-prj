8e9c9ccb3f47c24d725104b1908beea1f320654d
kmy1946 <kmy1946@outlook.jp>
2022-02-20 23:59:21 +0900

    '02202359_commit'

--- src/components/Products/ProductCard.js
+++ src/components/Products/ProductCard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core";
 import { Card } from "@material-ui/core";
 import { CardContent } from "@material-ui/core";
