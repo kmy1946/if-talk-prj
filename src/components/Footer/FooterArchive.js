efdf1335ed8d8fa9da2f36236b6390fdf5826d9a
kmy1946 <kmy1946@outlook.jp>
2022-03-07 00:03:59 +0900

    'categories_createdAtMonth_index_commit'

--- src/components/Footer/FooterArchive.js
+++ src/components/Footer/FooterArchive.js
@@ -9,9 +9,9 @@ const FooterArchive = () => {
     dispatch(push(path));//pathはvalueで指定
   };
   const [filters, setFilters] = useState([
-    {func: selectMenu, label: "2022.1", id: "2022_1_month", value: "/?updated_at_month=202201"},
-    {func: selectMenu, label: "2022.2", id: "2022_2_month", value: "/?updated_at_month=202202"},
-    {func: selectMenu, label: "2022.3", id: "2022_3_month", value: "/?updated_at_month=202203"},
+    {func: selectMenu, label: "2022.1", id: "2022_1_month", value: "/?created_at_month=202201"},
+    {func: selectMenu, label: "2022.2", id: "2022_2_month", value: "/?created_at_month=202202"},
+    {func: selectMenu, label: "2022.3", id: "2022_3_month", value: "/?created_at_month=202203"},
   ]);
 
   return (
